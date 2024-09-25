e788dd57efcfec455a05a8390ad391cbc7887ef2
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-09-25 13:29:02 +0200

    Added new file: docs/commands.sh

---
+++ docs/commands.sh
@@ -0,0 +1,19 @@
+# terraform init: Initializes a Terraform working directory. It downloads the provider plugins required for your configuration.
+terraform init
+
+# terraform plan: Previews the changes Terraform will make to the infrastructure based on the configuration files.
+terraform plan
+
+# terraform apply: Applies the changes required to reach the desired state of the configuration. It prompts for confirmation before proceeding.
+terraform apply
+
+# terraform destroy: Destroys all resources that were created by Terraform.
+terraform destroy 
+
+# terraform state: Allows inspection and modification of the Terraform state.
+terraform state
+
+# terraform refresh: Refreshes the state file to reflect the actual state of resources.
+terrafom refresh
+
+terraform console
