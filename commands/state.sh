f00c53d9aa176f12ac4d2e22bae144e494cf379f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:13:16 +0200

    Added new file: commands/

---
+++ commands/state.sh
@@ -0,0 +1,12 @@
+# Shows resources inside specified state file
+terraform state list
+
+# Move state
+terraform state mv
+
+# View content of remote state
+terraform state pull
+
+# Remote items from tfstate
+terraform state rm
+
