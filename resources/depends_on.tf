d8fca01bad05a3336fc785d65092e74a2c9229d0
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-25 22:07:15 +0100

    Modified: resources/depends_on.tf

--- resources/depends_on.tf
+++ resources/depends_on.tf
@@ -3,11 +3,32 @@
 # This is particularly useful when the dependency is not directly referenced within the resource itself.
 # Most of the times Terraform is able to automatically detect the order of dependency
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  depends_on    = [aws_vpc.my_vpc]
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_B1s" # Small VM, similar to t2.micro
+
+  admin_username = "azureuser"
+  admin_password = "P@ssw0rd1234!" # Replace with a secure password
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  depends_on = [azurerm_virtual_network.example]
 }
 
-# This ensures that the VPC is created before the EC2 instance is launched.
-
+# This ensures that the VNET is created before the VM
