de0b4b6b5b03e02603350c54229d83cdff5476a3
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-25 22:07:16 +0100

    Modified: resources/lifecycle_management.tf

--- resources/lifecycle_management.tf
+++ resources/lifecycle_management.tf
@@ -1,33 +1,40 @@
+
 # create_before_destroy
-# Terraform creates new resource before destroying the old one
-resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-
-  lifecycle {
-    create_before_destroy = true
-  } # This ensures the new EC2 instance is created first, avoiding downtime.
-}
-
-
 # ignore_changes
-# Prevents Terraform from trying to overwrite manual changes
-resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-
-  lifecycle {
-    ignore_changes = [ami]
-  } # If AMI is changed manually, Terraform will ignore that change and not attempt to modify it
-}
-
-
 # prevent_destroy
-# Avoid accidentaly destroying critical infrastructure
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-bucket"
+
+resource "azurerm_linux_virtual_machine" "example_vm" {
+  name                = "example-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+  admin_password = "P@ssw0rd1234!"  # Replace with a secure password
+
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 64  # OS disk size
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 
   lifecycle {
-    prevent_destroy = true
+    create_before_destroy = true  # Terraform creates new resource before destroying the old one
+
+    prevent_destroy = true        # Avoid accidentaly destroying critical infrastructure
+
+    ignore_changes = [            # Prevents Terraform from trying to overwrite manual changes
+      os_disk[0].disk_size_gb,    # If disk size is changed manually, Terraform will ignore that change and not attempt to modify it
+      admin_password 
+    ]
   }
 }
