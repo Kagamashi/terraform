86565072a782aeb3c97516f89edfe0dc206e5eef
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-25 22:07:15 +0100

    Modified: resources/for.tf

--- resources/for.tf
+++ resources/for.tf
@@ -1,33 +1,75 @@
-# Loop over a list and output a map
-# {for <ITEM> in <LIST> : <OUTPUT_KEY> => <OUTPUT_VALUE>}
-
-# Loop over a map and output a map
-# {for <KEY>, <VALUE> in <MAP> : <OUTPUT_KEY> => <OUTPUT_VALUE>}
-
-# Loop over a list to produce a transformed list
-# [for <ITEM> in <LIST> : <OUTPUT>]
-#   - LIST: A list to loop over
-#   - ITEM: Local variable name assigned to each item in LIST
-#   - OUTPUT: Expression that transforms ITEM in some way
 
 # A "for" expression creates a complex type value by transforming another complex type value.
 
-# Example: Transform a list of instance types to uppercase
-variable "instance_types" {
+# Loop over a list and output a map
+# {for <ITEM> in <LIST> : <OUTPUT_KEY> => <OUTPUT_VALUE>}
+variable "resource_names" {
   type    = list(string)
-  default = ["t2.micro", "t2.small", "t2.medium"]
+  default = ["app", "db", "cache"]
 }
 
-output "uppercase_instance_types" {
-  value = [for instance in var.instance_types : upper(instance)]  # Transform to uppercase
+output "resource_name_map" {
+  value = {for name in var.resource_names : name => upper(name)}
 }
+
+# {
+#   "app"   = "APP"
+#   "db"    = "DB"
+#   "cache" = "CACHE"
+# }
+
+
+
+# Loop over a map and output a map
+# {for <KEY>, <VALUE> in <MAP> : <OUTPUT_KEY> => <OUTPUT_VALUE>}
+variable "resource_types" {
+  type = map(string)
+  default = {
+    "web"  = "B1s"
+    "app"  = "B2s"
+    "db"   = "B2ms"
+  }
+}
+
+output "transformed_resource_types" {
+  value = {for type, size in var.resource_types : type => "Standard_${size}"}
+}
+
+# {
+#   "web"  = "Standard_B1s"
+#   "app"  = "Standard_B2s"
+#   "db"   = "Standard_B2ms"
+# }
+
+
+# Loop over a list to produce a transformed list
+# [for <ITEM> in <LIST> : <OUTPUT>]
+variable "resource_groups" {
+  type    = list(string)
+  default = ["rg-dev", "rg-test", "rg-prod"]
+}
+
+output "uppercase_resource_groups" {
+  value = [for rg in var.resource_groups : upper(rg)]
+}
+
+# ["RG-DEV", "RG-TEST", "RG-PROD"]
+
 
 # Example: Filter elements with a condition
-variable "instance_types" {
-  type    = list(string)
-  default = ["t2.micro", "t2.small", "t3.medium"]
+variable "resource_types" {
+  type = map(string)
+  default = {
+    "web"  = "B1s"
+    "app"  = "B2s"
+    "db"   = "B2ms"
+  }
 }
 
-output "filtered_instance_types" {
-  value = [for instance in var.instance_types : instance if instance != "t2.small"]
+output "filtered_resource_types" {
+  value = {for type, size in var.resource_types : type => size if size == "B2s"}
 }
+
+# {
+#   "app" = "B2s"
+# }
