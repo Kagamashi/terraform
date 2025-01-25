170e34185a1c3bc3b0b337a8e0d9af0f68c1ee14
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-25 20:24:51 +0100

    Modified: resources/for.tf

--- resources/for.tf
+++ resources/for.tf
@@ -1,17 +1,18 @@
-# Loop over a list and output a map 
-{for <ITEM> in <LIST> : <OUTPUT_KEY> => <OUTPUT_VALUE>} 
-  
-# Loop over a map and output a map 
-{for <KEY>, <VALUE> in <MAP> : <OUTPUT_KEY> => <OUTPUT_VALUE>} 
+# Loop over a list and output a map
+# {for <ITEM> in <LIST> : <OUTPUT_KEY> => <OUTPUT_VALUE>}
 
+# Loop over a map and output a map
+# {for <KEY>, <VALUE> in <MAP> : <OUTPUT_KEY> => <OUTPUT_VALUE>}
 
-[for <ITEM> in <LIST> : <OUTPUT>] 
-	• LIST - a list to loop over 
-	• ITEM - local variable name to assign to each item in LIST 
-OUTPUT - expression that transforms ITEM in some way 
+# Loop over a list to produce a transformed list
+# [for <ITEM> in <LIST> : <OUTPUT>]
+#   - LIST: A list to loop over
+#   - ITEM: Local variable name assigned to each item in LIST
+#   - OUTPUT: Expression that transforms ITEM in some way
 
-# For expression creates a complex type value by transforming another complex type value
+# A "for" expression creates a complex type value by transforming another complex type value.
 
+# Example: Transform a list of instance types to uppercase
 variable "instance_types" {
   type    = list(string)
   default = ["t2.micro", "t2.small", "t2.medium"]
@@ -21,9 +22,7 @@ output "uppercase_instance_types" {
   value = [for instance in var.instance_types : upper(instance)]  # Transform to uppercase
 }
 
-
-
-# Filtering elements with condition
+# Example: Filter elements with a condition
 variable "instance_types" {
   type    = list(string)
   default = ["t2.micro", "t2.small", "t3.medium"]
