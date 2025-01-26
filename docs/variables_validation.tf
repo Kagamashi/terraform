d76902726c743998677e48784b7010fe96a7c80e
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-26 12:41:21 +0100

    Modified: docs/variables_validation.tf

--- docs/variables_validation.tf
+++ docs/variables_validation.tf
@@ -32,7 +32,7 @@ variable "environment" {
   }
 }
 
-# Validating numeric ranges
+# validate numeric ranges
 variable "instance_count" {
   type        = number
   description = "The number of EC2 instances to launch"
@@ -43,7 +43,7 @@ variable "instance_count" {
   }
 }
 
-# Ensuring a list has a minimum number of elements
+# ensuring a list has a minimum number of elements : length()
 variable "allowed_ips" {
   type        = list(string)
   description = "List of allowed IP addresses"
@@ -54,7 +54,7 @@ variable "allowed_ips" {
   }
 }
 
-# Ensuring a value is a valid boolean
+# ensuring a value is a valid boolean
 variable "enable_monitoring" {
   type        = bool
   description = "Whether to enable detailed monitoring on the instance"
@@ -65,7 +65,7 @@ variable "enable_monitoring" {
   }
 }
 
-# Validating a map has specific keys
+# validating a map has specific keys : alltrue() keys() contains()
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to resources"
@@ -75,9 +75,6 @@ variable "tags" {
     error_message = "The 'tags' map must contain both 'Name' and 'Environment' keys."
   }
 }
-/* keys() function extracts keys from the tags map
-contains() function checks if the required keys (Name and Environment) are present
-alltrue() functions ensures all conditions are met */
 
 # validating a complex object
 variable "autoscaling_config" {
@@ -90,6 +87,5 @@ variable "autoscaling_config" {
   validation {
     condition     = var.autoscaling_config.min_size <= var.autoscaling_config.desired_capacity && var.autoscaling_config.desired_capacity <= var.autoscaling_config.max_size
     error_message = "Desired capacity must be between min_size and max_size."
-  }
+  } # validation ensures that desired_capacity is within min_size and max_size bounds
 }
-# validation ensures that desired_capacity is within min_size and max_size bounds
