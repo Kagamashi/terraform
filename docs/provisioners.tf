62d77fade2a381b085e9a48a8c7996e94784059f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-26 12:26:49 +0100

    Modified: docs/provisioners.tf

--- docs/provisioners.tf
+++ docs/provisioners.tf
@@ -1,10 +1,14 @@
-/* Provisioners in Terraform allow you to run scripts or commands on resources, either remotely (remote-exec) or locally (local-exec).
-Common Use Cases:
-  Installing software on a VM after it has been provisioned.
-  Running configuration scripts.
-  Bootstrapping instances with necessary configuration.
-  Executing local actions (on the machine where Terraform is run) as part of the resource creation process. */
+/* 
+Provisioners in Terraform allow you to run scripts or commands on resources, either remotely (remote-exec) or locally (local-exec).
+Use cases:
+  - Installing software on a VM after it has been provisioned.
+  - Running configuration scripts.
+  - Bootstrapping instances with necessary configuration.
+  - Executing local actions (on the machine where Terraform is run) as part of the resource creation process.
 
+Downsides: Provisioners are not idempotent, difficult to track, and can lead to inconsistent states if they fail.
+Alternatives: Use configuration management tools like Ansible || Chef || Puppet || cloud-init || managed services to avoid the complexities of provisioners. 
+*/
 
 # remote-exec is commonly used for running scripts on remote machines
 resource "aws_instance" "example" {
@@ -37,8 +41,3 @@ resource "aws_instance" "example" {
     command = "echo 'Instance ID is ${self.id}'"  # self reference to access resource attributes
   }
 }
-
-# Downsides: Provisioners are not idempotent, difficult to track, and can lead to inconsistent states 
-# if they fail, making them a less desirable solution for most infrastructure automation tasks.
-
-# Alternatives: Use configuration management tools like Ansible, cloud-init, or managed services to avoid the complexities of provisioners.
